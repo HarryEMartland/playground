25e3733e7bc9eca1afb1af7b76f3c22e77bc1f3d
martlandh <HarryMartland@Rentalcars.com>
2016-09-22 23:33:47 +0100

    added examples of ignoring fields for jackson

---
+++ src/test/java/uk/co/harrymartland/playground/jacksonPlayground.java
@@ -0,0 +1,52 @@
+package uk.co.harrymartland.playground;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class jacksonPlayground {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void shouldIgnoreFields1To3() throws JsonProcessingException {
+        ExampleClass exampleClass = new ExampleClass();
+        Assert.assertEquals("{\"field4\":\"value4\"}", objectMapper.writeValueAsString(exampleClass));
+    }
+
+    private static class SuperClass {
+        private String field1 = "value1";
+        private String field2 = "value2";
+        private String field3 = "value3";
+
+        public String getField1() {
+            return field1;
+        }
+
+        public String getField2() {
+            return field2;
+        }
+
+        public String getField3() {
+            return field3;
+        }
+    }
+
+    @JsonIgnoreProperties(value = {"field1", "field2"})
+    private static class ExampleClass extends SuperClass {
+        private String field4 = "value4";
+
+        @Override
+        @JsonIgnore
+        public String getField3() {
+            return super.getField3();
+        }
+
+        public String getField4() {
+            return field4;
+        }
+    }
+}
